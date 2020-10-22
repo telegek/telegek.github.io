427d13b75ee546ee77315fd4acd47e6d64d524ef
Kuat <kuat.telegenov@kaust.edu.sa>
2020-10-22 15:52:30 +0300

    commit

--- script.js
+++ script.js
@@ -34,8 +34,10 @@ d3.csv('data/data.csv')
 .then(function(data) {
   const columns = ['Instrument/ISIN','Quantity', 'Price', 'Direction', 'Trading day']
   // tabulate(data,columns)
-  data.Quantity = data.Quantity.str.slice(0, -1)
-  tabulate(data,columns)
+  
+
+  console.log(data.Quantity[0]);
+  // tabulate(data,columns)
   // byticker = d3.group(data, d => d.'Instrument/ISIN')
   // tabulate(byticker, columns)
 });
