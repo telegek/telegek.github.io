5b0985eacc33eb4d9010cd4b2365f027d5b39699
Kuat <kuat.telegenov@kaust.edu.sa>
2020-10-22 13:55:30 +0300

    commit

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 var tabulate = function (data,columns) {
-    var table = d3.select('body').append('table')
-      var thead = table.append('thead')
-      var tbody = table.append('tbody')
-  
-      thead.append('tr')
-        .selectAll('th')
-          .data(columns)
-          .enter()
-        .append('th')
-          .text(function (d) { return d })
-  
-      var rows = tbody.selectAll('tr')
-          .data(data)
-          .enter()
-        .append('tr')
-  
-      var cells = rows.selectAll('td')
-          .data(function(row) {
-              return columns.map(function (column) {
-                  return { column: column, value: row[column] }
-            })
-        })
+  var table = d3.select('#myTable') // this is the solution
+// var table = d3.select('body').append('table') this was before the solution
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+
+
+    thead.append('tr')
+      .selectAll('th')
+        .data(columns)
         .enter()
-      .append('td')
-        .text(function (d) { return d.value })
-  
-    return table;
-  }
-  
-  d3.csv('csv/data2.csv',function (data) {
-      var columns = ['variable','aror','asd','maxdd']
-    tabulate(data,columns)
-  })
+      .append('th')
+        .text(function (d) { return d })
+
+    var rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+      .append('tr')
+
+    var cells = rows.selectAll('td')
+        .data(function(row) {
+            return columns.map(function (column) {
+                return { column: column, value: row[column] }
+          })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+d3.csv('data/data.csv')
+.then(function(data) {
+  const columns = ['Quantity', 'Price']
+  tabulate(data,columns)
+});
