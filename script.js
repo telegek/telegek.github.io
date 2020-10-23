335f5d6e16f747bbb3d8c0d954836ea634206576
Kuat <kuat.telegenov@kaust.edu.sa>
2020-10-23 18:44:21 +0300

    commit

--- script.js
+++ script.js
@@ -78,10 +78,14 @@ d3.csv('data/data.csv')
 
 
 
-  map = d3.rollups(data, v => d3.sum(v, d => d["Total amount"]), d => d["Instrument/ISIN"]);
-
-  console.log(map[0][0])
+  total_amount = d3.rollups(data, v => d3.sum(v, d => d["Total amount"]), d => d["Instrument/ISIN"]);
+  quantity = d3.rollups(data, v => d3.sum(v, d => d["Quantity"]), d => d["Instrument/ISIN"]);
   
+  sorted_total_amount = total_amount.slice().sort((a, b) => d3.descending(a["Total amount"], b["Total amount"]))
+  // map.sort(d3.ascending)
+
+  console.log(sorted_total_amount)
+
   createTable(map);
   make_single_stocks()
 });
