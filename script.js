4a4ab03f8ca53dc6c336c9e223a6ba1091702484
Kuat <kuat.telegenov@kaust.edu.sa>
2020-10-25 09:14:42 +0300

    commit

--- script.js
+++ script.js
@@ -89,4 +89,38 @@ d3.csv('data/data.csv')
 
   createTable(sorted_total_amount);
   make_single_stocks()
+
+
+
+
+
+
+
+
+
+
+
+
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work',     11],
+      ['Eat',      2],
+      ['Commute',  2],
+      ['Watch TV', 2],
+      ['Sleep',    7]
+    ]);
+
+    var options = {
+      title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+  }
 });
